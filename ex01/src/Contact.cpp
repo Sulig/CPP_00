a14f01d9934d47ad36546a37d2f4013fcecb5987
Sasha Domínguez Ferrer <sadoming@student.42barcelona.com>
2025-01-22 18:25:06 +0100

    Class Contact finished for now

--- ex01/src/Contact.cpp
+++ ex01/src/Contact.cpp
@@ -6,20 +6,69 @@
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:36:35 by sadoming          #+#    #+#             */
-/*   Updated: 2024/09/17 17:36:35 by sadoming         ###   ########.fr       */
+/*   Updated: 2025/01/22 18:22:37 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Contact.hpp"
+#include "./inc/Contact.hpp"
 
 Contact::Contact(void) { return ; }
 Contact::~Contact(void) { return ; }
 
-Contact::Contact(std::string first_name, std::string last_name, std::string nickname, std::string phone, std::string secret;) 
+Contact::Contact(std::string firstName, std::string lastName, std::string nickname, int phone, std::string secret)
 {
-    this->first_name = first_name;
-    this->last_name = last_name;
-    this->nickname = nickname;
-    this->phone = phone;
-    this->secret = secret;
+	this->firstName = firstName;
+	this->lastName = lastName;
+	this->nickname = nickname;
+	this->phone = phone;
+	this->secret = secret;
+}
+
+int	Contact::isNull(Contact contact)
+{
+	return (contact.firstName.empty());
+}
+
+void	Contact::displayLargeStrings(std::string toPrint)
+{
+	if (toPrint.length() > 10)
+	{
+		//Declare special type of string that will recive the toPrint cuted with max width of 10
+		std::stringstream	tmp;
+		tmp << std::setw(10) << toPrint;
+		// Convert the stream to string, so it will can be printed
+		std::cout << tmp.str();
+	}
+	else
+	{
+		// How many spaces has to print?
+		int		spaces = 10 - toPrint.length();
+		// Print spaces
+		for (spaces > 0; spaces--;)
+			std::cout << ' ';
+		// Print it.
+		std::cout << toPrint;
+	}
+}
+
+void	Contact::displayInSearch(Contact contact, int index)
+{
+	std::cout << "|         " << index;
+	displayLargeStrings(contact.firstName);
+	std::cout << "|";
+	displayLargeStrings(contact.lastName);
+	std::cout << "|";
+	displayLargeStrings(contact.nickname);
+	std::cout << "|" << std::endl;
+}
+
+void	Contact::displayAll(Contact contact)
+{
+	std::cout << "[ Contact information ]" << std::endl;
+	std::cout << "|| First name:\t" << contact.firstName << std::endl;
+	std::cout << "|| Last name: \t" << contact.lastName << std::endl;
+	std::cout << "|| Nickname:  \t" << contact.nickname << std::endl;
+	std::cout << "|| Phone:\t\t\t" << contact.phone << std::endl;
+	std::cout << "|| Darkest Secret:\t" << contact.secret << std::endl;
+	std::cout << "~ ------------------" << std::endl << std::endl;
 }
--- ex01/src/inc/Contact.hpp
+++ ex01/src/inc/Contact.hpp
@@ -6,29 +6,35 @@
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:40:45 by sadoming          #+#    #+#             */
-/*   Updated: 2025/01/21 17:26:49 by sadoming         ###   ########.fr       */
+/*   Updated: 2025/01/22 18:05:15 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 # define CONTACT_HPP
 
-# include <string>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <string>
 
-class Contact:
+class Contact
 {
 	public:
-		Contact();
+		Contact(void);
 		Contact(std::string firstName, std::string lastName, std::string nickname, int phone,
 		std::string secret);
 		~Contact();
+		int		Contact::isNull(Contact contact);
+		void	Contact::displayInSearch(Contact contact, int index);
+		void	Contact::displayAll(Contact contact);
 	private:
 		std::string	firstName;
 		std::string	lastName;
 		std::string	nickname;
 		int			phone;
 		std::string	secret;
-		void modifyDataContact();
+		void	Contact::displayLargeStrings(std::string toPrint);
 };
 
 #endif
