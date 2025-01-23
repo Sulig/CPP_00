801e119e98c699efb308c1ba00c82457da599263
Sadoming <suligvondooka@gmail.com>
2025-01-23 20:00:54 +0100

    Update AwesomePhoneBookMain.cpp

--- ex01/src/AwesomePhoneBookMain.cpp
+++ ex01/src/AwesomePhoneBookMain.cpp
@@ -6,23 +6,19 @@
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:37:49 by sadoming          #+#    #+#             */
-/*   Updated: 2025/01/21 16:38:48 by sadoming         ###   ########.fr       */
+/*   Updated: 2025/01/23 19:50:01 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "./inc/PhoneBook.hpp"
-//#include "./inc/Contact.hpp"
-
-#include <cstdlib>
-#include <iostream>
-#include <string>
+#include "./inc/PhoneBook.hpp"
+#include "./inc/Contact.hpp"
 
 /* Manage User Input */
 /* The EOF is controlled by using:
 *	- `std::getline(std::cin, userInput);` To get the input
 *	- `std::cin.eof()` Gets the status of .eofbit
 */
-void	manageUserInput()
+void	manageUserInput(PhoneBook phoneBook)
 {
 	std::string	userInput;
 
@@ -33,15 +29,12 @@ void	manageUserInput()
 		exit(0);
 	}
 	else if (userInput == "ADD")
-	{
-
-	}
+		phoneBook.addContact(phoneBook);
 	else if (userInput == "SEARCH")
-	{
-
-	}
+		phoneBook.showContacts(phoneBook);
 	else if (userInput == "EXIT")
 	{
+		std::cout << "No more contacts? @_)@" << std::endl;
 		std::cout << "ALL INFORMATION WILL BE CLEANED.. EXITING.." << std::endl;
 		exit(0);
 	}
@@ -51,14 +44,14 @@ void	manageUserInput()
 
 int main()
 {
+	PhoneBook	phoneBook;
 
-
+	std::cout << "[[ PHONEBOOK ] [ V 0.3 ]]" << std::endl;
 	while (true)
 	{
-		std::cout << "[[ PHONEBOOK ] [ V 0.1 ]]" << std::endl;
 		std::cout << "> Please enter an action | Options below" << std::endl;
 		std::cout << "| ADD | SEARCH | EXIT |" << std::endl;
-		manageUserInput();
+		manageUserInput(phoneBook);
 	}
 	return (0);
 }
