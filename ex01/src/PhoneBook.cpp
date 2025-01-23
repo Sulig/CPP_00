c73c4e18a04f7cc5e9372cb124040d229d57dfca
Sadoming <suligvondooka@gmail.com>
2025-01-23 20:00:29 +0100

    Update PhoneBook.cpp

--- ex01/src/PhoneBook.cpp
+++ ex01/src/PhoneBook.cpp
@@ -6,23 +6,60 @@
 /*   By: sadoming <sadoming@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:36:56 by sadoming          #+#    #+#             */
-/*   Updated: 2025/01/23 18:00:54 by sadoming         ###   ########.fr       */
+/*   Updated: 2025/01/23 19:59:51 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PhoneBook.hpp"
+#include "./inc/PhoneBook.hpp"
 
-PhoneBook::PhoneBook() { return ;}
-PhoneBook::~PhoneBook() {
-	std::cout << "No more contacts? 0_)0" << std::endl;
-	return ;}
+PhoneBook::PhoneBook(void) { return ;}
+PhoneBook::~PhoneBook(void) { return ; }
 
 /* Remove innecesary block of chars */
 std::string	PhoneBook::trim(std::string str)
 {
+	if (std::cin.eof())
+		return (str);
 	str.erase(0, str.find_first_not_of(" \t\v\r\f\n"));
 	str.erase(str.find_last_not_of(" \t\v\r\f\n") + 1);
 	return (str);
+}
+
+int	PhoneBook::atoi(std::string str)
+{
+	int		cnt;
+	int		sign;
+	int		find;
+
+	find = 0;
+	cnt = -1;
+	sign = 1;
+	while (str[++cnt] < 33)
+		if ((str[cnt] < 9 || str[cnt] > 13) && str[cnt] != ' ')
+			return (-1);
+	if (!(str[cnt] >= '0' && str[cnt] <= '9'))
+		return (-1);
+	if (str[cnt] == '-' || str[cnt] == '+')
+	{
+		if (str[cnt] == '-')
+			sign *= -1;
+		cnt++;
+	}
+	while (str[cnt] >= '0' && str[cnt] <= '9')
+	{
+		find *= 10;
+		find += str[cnt] - '0';
+		cnt++;
+	}
+	return (find * sign);
+}
+
+int	PhoneBook::lastContact(PhoneBook phoneBook)
+{
+	for (int i = 0; i < 8; i++)
+		if (phoneBook.contacts[i].isNull(phoneBook.contacts[i]))
+			return (i);
+	return (8);
 }
 
 std::string	PhoneBook::manageInput(void)
@@ -31,10 +68,8 @@ std::string	PhoneBook::manageInput(void)
 
 	do {
 		std::getline(std::cin, userInput);
-		if (std::cin.eof() || !userInput.length())
-			std::cout << "Please introduce something!" << std::endl;
 		userInput = trim(userInput);
-		if (!userInput.length())
+		if (!userInput.length() || std::cin.eof())
 			std::cout << "Please introduce something!" << std::endl;
 		else
 			break;
@@ -45,7 +80,7 @@ std::string	PhoneBook::manageInput(void)
 void	PhoneBook::showContacts(PhoneBook phoneBook)
 {
 	std::cout << "[ Actual Contacts ]" << std::endl;
-	if (!phoneBook.contacts)
+	if (phoneBook.contacts[0].isNull(phoneBook.contacts[0]))
 		std::cout << "- Oh! No contacts! -" << std::endl;
 	else //Prompt Contact list
 	{
@@ -53,14 +88,16 @@ void	PhoneBook::showContacts(PhoneBook phoneBook)
 		std::string input;
 
 		std::cout << "     Index|First Name| Last Name|  Nickname" << std::endl;
-		for (int i = 0; i < phoneBook.lastContact; i++) {
+		for (int i = 0; i < phoneBook.lastContact(phoneBook); i++) {
+			if (i >=8 || phoneBook.contacts[i].isNull(phoneBook.contacts[i]))
+				break ;
 			phoneBook.contacts[i].displayInSearch(phoneBook.contacts[i], i);
 		}
 		std::cout << "[//*************************//]" << std::endl << std::endl;
 		std::cout << "To see more details of some contact, insert the index number" << std::endl;
 		input = manageInput();
-		index = std::stoi(input);
-		if (index >= 0 && index < phoneBook.lastContact)
+		index = atoi(input);
+		if (index >= 0 && index < phoneBook.lastContact(phoneBook))
 			phoneBook.contacts[index].displayAll(phoneBook.contacts[index]);
 		else
 			std::cout << "Invalid index" << std::endl;
@@ -86,25 +123,23 @@ PhoneBook	PhoneBook::addContact(PhoneBook phoneBook)
 	nickname = manageInput();
 	std::cout << "> Add the Phone number: " << std::endl;
 	tmPhone = manageInput();
-	phone = std::stoi(tmPhone);
-	while (!(phone >= 0))
+	phone = atoi(tmPhone);
+	while (phone < 0)
 	{
 		std::cout << "Invalid phone number" << std::endl;
 		std::cout << "> Add a VALID Phone number: " << std::endl;
 		tmPhone = manageInput();
-		phone = std::stoi(tmPhone);
+		phone = atoi(tmPhone);
 	}
 	std::cout << "> Add the Darkest secret: " << std::endl;
 	secret = manageInput();
 	std::cout << "[> Creating contact.. :) ]" << std::endl;
+
 	contact = Contact(firstName, lastName, nickname, phone, secret);
 
 	// Add contact to the phonebook
-	if (lastContact < 8)
-	{
-		phoneBook.contacts[lastContact] = contact;
-		lastContact++;
-	}
+	if (phoneBook.lastContact(phoneBook) < 8)
+		phoneBook.contacts[phoneBook.lastContact(phoneBook)] = contact;
 	/* If the phonebook is full, remove the last contact
 		and add the new one to first position ~
 	*/
